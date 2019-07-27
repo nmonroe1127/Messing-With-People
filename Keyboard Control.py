02ce2463d64ca6d648b5c08580d66e5f66e86377
nmonroe1127 <39540278+nmonroe1127@users.noreply.github.com>
2019-07-27 16:35:56 -0500

    Update Keyboard Control.py

--- Keyboard Control.py
+++ Keyboard Control.py
@@ -8,7 +8,13 @@ import pyttsx3
 ## Opening up a google webpage and then sleeping for ten seconds so that it can load properly
 visit = "http://{}".format('google.com')
 webbrowser.open(visit)
-time.sleep(10)
+sleep_length = 0
+while sleep_length < 100:
+    pos_x = random.randrange(300, 900)
+    pos_y = random.randrange(200, 700)
+    autopy.mouse.move(pos_x, pos_y)
+    time.sleep(1/10)
+    sleep_length += 1
 
 ## This is the first string that will be auto-typed into the google search bar.
 string_warning = "Hi there. I hope you realize that I have complete control of your computer now." \
@@ -18,6 +24,9 @@ string_position = 0
 while string_position < len(string_warning):
     pyautogui.press(string_warning[string_position])
     string_position += 1
+    pos_x = random.randrange(300, 900)
+    pos_y = random.randrange(200, 700)
+    autopy.mouse.move(pos_x, pos_y)
 
 ## We then have a separate string for the countdown. Just made a second for clarity
 string_countdown = "5..........4..........3..........2..........1.........."
@@ -25,6 +34,9 @@ string_position = 0
 while string_position < len(string_countdown):
     pyautogui.press(string_countdown[string_position])
     string_position += 1
+    pos_x = random.randrange(300, 900)
+    pos_y = random.randrange(200, 700)
+    autopy.mouse.move(pos_x, pos_y)
 
 ## lets mess with people and have the speaker randomly start talking to them.
 engine = pyttsx3.init()
@@ -32,18 +44,18 @@ engine.say('Well, I guess we all make mistakes sometimes. Im gonna make your com
 engine.runAndWait()
 
 ## Now we can choose random sites and open them, also the mouse will spaz out.
-# page_total = 0
-# while page_total < 30:
-#     sites = random.choice(['google.com/search?q=you+are+being+hacked',
-#                            'google.com/search?q=just+give+up',
-#                            'google.com/search?q=never+click+links',
-#                            'youtube.com/results?search_query=you+have+been+hacked'])
-#     visit = "http://{}".format(sites)
-#     webbrowser.open(visit)
-#     seconds = random.randrange(5, 10)
-#
-#     time.sleep(5/100)
-#     page_total += 1
-#     pos_x = random.randrange(300, 900)
-#     pos_y = random.randrange(200, 700)
-#     autopy.mouse.move(pos_x, pos_y)
+page_total = 0
+while page_total < 30:
+    sites = random.choice(['google.com/search?q=you+are+being+hacked',
+                           'google.com/search?q=just+give+up',
+                           'google.com/search?q=never+click+links',
+                           'youtube.com/results?search_query=you+have+been+hacked'])
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5, 10)
+
+    time.sleep(5/100)
+    page_total += 1
+    pos_x = random.randrange(300, 900)
+    pos_y = random.randrange(200, 700)
+    autopy.mouse.move(pos_x, pos_y)
